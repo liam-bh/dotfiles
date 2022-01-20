f7bf7468f9a731afd8430a31bebc648165c62021
Liam B <runningzoomcat@gmail.com>
2022-01-20 17:20:23 +1100

    remove --all flag

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
-    alias lss "exa -l --no-user --no-time --no-filesize --all --octal-permissions --no-permissions"
+    alias lss "exa -l --no-user --no-time --no-filesize --octal-permissions --no-permissions"
     alias c clear
+    alias all "nvim -p *"
     set fish_greeting ""
 end
